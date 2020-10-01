1e78e454cf4c68da3f87a998d47a4c8f914ec203
Jelle De Coninck <jelle.deconinck@student.kuleuven.be>
2020-10-01 23:49:02 +0200

    Added two extra tests showcasing order of evaluation
    - added a test with an infinite loop that won't be evaluated under lazy
      evaluation
    - added a test with fibonacci numbers
    
    Note that the infinite loop test also exposes an existing bug in KHC,
    i.e. it passes the frontend typecheck but not the System F typecheck.

---
+++ Tests/Test3.hs
@@ -0,0 +1,17 @@
+data Nil = Nil
+
+class Fst a :: * where
+  fst :: forall (b :: *). forall (c :: *). b -> c -> b
+class Snd a :: * where  
+  snd :: forall (b :: *). forall (c :: *). b -> c -> c
+class Loop a :: * where  
+  loop :: a
+
+instance Fst Nil where
+  fst = \x. \y. x
+instance Snd Nil where
+  snd = \x. \y. y
+instance Loop Nil where
+  loop = loop
+
+fst (\x. x) loop
