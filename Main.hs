6e13029a51fafc7f636804b42ccff1ee78522839
stormeuh <elias.storme@gmail.com>
2021-04-17 13:57:26 +0200

    first version of optimizer elaboration

--- Main.hs
+++ Main.hs
@@ -5,8 +5,8 @@ module Main (main, runTest) where
 import Frontend.HsParser      (hsParse)
 import Frontend.HsRenamer     (hsRename)
 import Frontend.HsTypeChecker (hsElaborate)
-import Optimizer.FcTypeChecker  (fcTypeCheck)
-import Backend.Interpreter.FcEvaluator    (fcEvaluate)
+import Optimizer.FcTypeChecker  (fcOptElaborate)
+-- import Backend.Interpreter.FcEvaluator    (fcEvaluate)
 
 import Utils.Unique  (newUniqueSupply)
 import Utils.PrettyPrint
@@ -32,21 +32,23 @@ runTest file = do
         (Right (((rn_pgm, _rn_ctx), us1), rn_env), _) ->
           case hsElaborate rn_env us1 rn_pgm of
             (Left err,_) -> throwMainError "typechecker" err
-            (Right ((((fc_pgm, tc_ty, theory), envs), us2), _tc_env), _) -> do
+            (Right ((((fc_opt_pgm, tc_ty, theory), envs), us2), _tc_env), _) -> do
               putStrLn "---------------------------- Elaborated Program ---------------------------"
-              putStrLn $ renderWithColor $ ppr fc_pgm
+              putStrLn $ renderWithColor $ ppr fc_opt_pgm
               putStrLn "------------------------------- Program Type ------------------------------"
               putStrLn $ renderWithColor $ ppr tc_ty
               putStrLn "------------------------------ Program Theory -----------------------------"
               putStrLn $ renderWithColor $ ppr theory
-              case fcTypeCheck envs us2 fc_pgm of
+              case fcOptElaborate envs us2 fc_opt_pgm of
                 (Left err,_) -> throwMainError "System F typechecker" err
-                (Right ((fc_ty, us3), _fc_env), _trace) -> do
-                  putStrLn "-------------------------- System F Program Type --------------------------"
+                (Right (((fc_ty, fc_res_pgm), us3), _fc_env), _trace) -> do
+                  putStrLn "--------------------- System F Optimizer Program Type ---------------------"
                   putStrLn $ renderWithColor $ ppr fc_ty
-                  let res = fcEvaluate us3 fc_pgm
-                  putStrLn "-------------------------- System F Result --------------------------------"
-                  putStrLn $ renderWithColor $ ppr res
+                  putStrLn "----------------------- System F Restricted Program -----------------------"
+                  putStrLn $ renderWithColor $ ppr fc_res_pgm
+                  -- let res = fcEvaluate us3 fc_pgm
+                  -- putStrLn "-------------------------- System F Result --------------------------------"
+                  -- putStrLn $ renderWithColor $ ppr res
 
   where
     throwMainError phase e
